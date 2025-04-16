a18e20a6145f8caacea70697697ca0d017304b29
Ted Fulmer <ripvansabre@gmail.com>
2025-04-16 18:39:29 -0500

    expanded the members of $pattern

--- recentMods.ps1
+++ recentMods.ps1
@@ -11,13 +11,13 @@ write-host "Looking for files modified since $TARGETDATE"
 # if ( $number > 0 ) { 
 # write-host "$newNumber"
 
-$pattern = 'FaceCollection|Applications|Music|Movies|Photos Library\.photoslibrary|AudioBooks|Microsd32|fromCD|\.vscode'
+$pattern = 'FaceCollection|Applications|Music|Movies|Photos Library\.photoslibrary|\.max|\.scriv|AudioBooks|\.log|Microsd32|fromCD|\.vscode'
 
 Get-ChildItem -File -Recurse |
     Where-Object {
         $_.LastWriteTime -gt $TARGETDATE -and
         $_.FullName -notmatch $pattern
     } |
-    ForEach-Object { dir $_.FullName }
+    ForEach-Object {  $_.FullName }
 
 write-host "Files modified since $TARGETDATE"
